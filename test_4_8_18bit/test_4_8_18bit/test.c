a937cf7952e460701c888c984cf03233bd597923
whjlearn <858992079@qq.com>
2023-04-08 23:18:41 +0800

    整形提升，float在内存中的存储

---
+++ test_4_8_18bit/test_4_8_18bit/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int n = 10;
+//	unsigned int m = -20;
+//	printf("%d", n - m);
+//
+//	return 0;
+//}
+//int main()
+//{
+//	float n = 9.25;
+//
+//	return 0;
+//}
