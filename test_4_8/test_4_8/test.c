21af520597ce1bafcf5d8aae90146f2c0f083eca
whjlearn <858992079@qq.com>
2023-04-08 14:39:03 +0800

    自我实现strlen，自我实现strcmp函数

---
+++ test_4_8/test_4_8/test.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int my_strlen(char* src)
+//{
+//	int count = 0;
+//	while (*src++!='\0')
+//	{
+//		count++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	//自我实现strlen
+//	printf("%d\n",my_strlen("abcdfert"));
+//	return 0;
+//}
+
+//自我实现strcmp函数
+//#include<stdio.h>
+//#include<assert.h>
+//char* my_strcmp(char* dest, char* src)
+//{
+//	assert(dest&&src);//断言
+//	char* ret = dest;
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	char arr[] = "adffasdfasdf";
+//	char arr1[20];
+//	my_strcmp(arr1,arr);
+//	printf("%s", arr1);
+//	return 0;
+//}
